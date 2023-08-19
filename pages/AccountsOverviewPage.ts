0ec6b3a885efc6817b52025c813de614910e5d3b
Iroshan Vithanage <iroshanvithanage@Iroshans-MacBook-Air.local>
2023-08-19 14:59:20 +0530

    improved code quality

--- pages/AccountsOverviewPage.ts
+++ pages/AccountsOverviewPage.ts
@@ -1,30 +1,21 @@
 import { Locator, Page } from "@playwright/test";
 
 export class AccountsOverviewPage {
-
   readonly page: Page;
- 
   readonly lblWelcome: Locator;
 
 
-  
-  constructor(page: Page){
+
+  constructor(page: Page) {
     this.page = page;
-
     this.lblWelcome = page.locator('//p[@class = "smallText"]/b');
-
-}
-   
+  }
 
 
-    async isValidLoginLblVisible(){
-      await this.lblWelcome.waitFor({state: "visible"});
-      return  this.lblWelcome.isVisible();
-    }
 
-     async visit(){
-      await this.page.goto("https://parabank.parasoft.com/parabank/index.htm");     
-    }
+  async visit() {
+    await this.page.goto("https://parabank.parasoft.com/parabank/index.htm");
+  }
 
-    
+
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,43 +3,43 @@ import { LoginPage } from '../pages/LoginPage';
 import { AccountsOverviewPage } from '../pages/AccountsOverviewPage';
 
 test.describe("Login Page Tests", () => {
- 
-let loginPage: LoginPage;
-let accoutsOverviewPage : AccountsOverviewPage;
+
+  let loginPage: LoginPage;
+  let accoutsOverviewPage: AccountsOverviewPage;
 
 
 
-test.beforeEach(({page}) => {
-  loginPage = new LoginPage(page);
-  accoutsOverviewPage = new AccountsOverviewPage(page);
-});
-
-
-
-test("TC-001: Checking whether user can logged in", async({page}) => {
-  await test.step("1.Log in to the site", async() => {
-    await loginPage.visit();
-  });
-  await test.step("2.Enter correct username and password", async() => {
-    await loginPage.login("Admin","admin123");
-  });
-  await test.step("3.Validate user logged in successfuly", async() => {
-    await expect( await accoutsOverviewPage.isValidLoginLblVisible()).toBeTruthy();
-  });
-});
-
-
-test("TC-002: Checking whether wrong username, password does not allow users to log in", async({page}) => {
-  await test.step("1.Log in to the site", async() => {
-    await loginPage.visit();
-  });
-  await test.step("2.Enter correct username and password", async() => {
-    await loginPage.login("Admin","");
-  });
-  await test.step("3.Validate Incorrect username and password message is visible", async() => {
-    await expect( await loginPage.isInvalidLoginLblVisible()).toBeTruthy();
+  test.beforeEach(({ page }) => {
+    loginPage = new LoginPage(page);
+    accoutsOverviewPage = new AccountsOverviewPage(page);
   });
 
-});
+
+
+  test("TC-001: Checking whether user can logged in", async ({ page }) => {
+    await test.step("1.Log in to the site", async () => {
+      await loginPage.visit();
+    });
+    await test.step("2.Enter correct username and password", async () => {
+      await loginPage.login("Admin", "admin123");
+    });
+    await test.step("3.Validate user logged in successfuly", async () => {
+      await expect(await accoutsOverviewPage.lblWelcome).toBeVisible({ timeout: 5000 });
+    });
+  });
+
+
+  test("TC-002: Checking whether wrong username, password does not allow users to log in", async ({ page }) => {
+    await test.step("1.Log in to the site", async () => {
+      await loginPage.visit();
+    });
+    await test.step("2.Enter correct username and password", async () => {
+      await loginPage.login("Admin", "");
+    });
+    await test.step("3.Validate Incorrect username and password message is visible", async () => {
+      await expect(loginPage.lblInvalidLogin).toBeVisible({ timeout: 5000 });
+    });
+
+  });
 
 });
